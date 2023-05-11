346035e19c9d6381ae0278d716a7fbb7f99036fd
Griff2005 <griffrob1205@gmail.com>
2023-05-11 12:51:25 -0400

    Setup

---
+++ DutyScheduler/src/Sheduler/Window.java
@@ -0,0 +1,32 @@
+package Sheduler;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @author: Griffin Robinson
+ * @date: 2023-05-11
+ * @File Name: Window
+ * @description: _______
+ */
+public class Window {
+    private JPanel StartPage;
+    private JPanel SheduleGeneratorPage;
+    private JButton button1;
+
+    private CardLayout cardLayout = new CardLayout();
+
+public Window() {
+
+    
+
+    button1.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    });
+}
+}
