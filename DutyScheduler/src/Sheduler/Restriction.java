a9631f0c022cdd8dbb5701fb68683de506df737e
Griff2005 <griffrob1205@gmail.com>
2023-05-23 12:03:03 -0400

    Final code template setup

--- DutyScheduler/src/Sheduler/Restriction.java
+++ DutyScheduler/src/Sheduler/Restriction.java
@@ -1,5 +1,7 @@
 package Sheduler;
 
+import java.util.Date;
+
 /**
  * The type Restriction. TODO: Add description
  *
@@ -8,5 +10,49 @@ package Sheduler;
  * @File: Restriction
  */
 public class Restriction {//todo: Error Trapping/Handling
-    //todo add methods and fields
+    private Teacher teacher;
+    private String description;
+    private Date date;
+    private Time time;
+
+    public Restriction(Teacher teacher, String description, Date date, Time time) {
+        this.teacher = teacher;
+        this.description = description;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+
 }
