b0425c41b9979a93e973a045182f515014478a50
Griff2005 <griffrob1205@gmail.com>
2023-06-01 12:11:04 -0400

    Abstraction/Inheritance additions

---
+++ DutyScheduler/src/Sheduler/SupervisionDuty.java
@@ -0,0 +1,21 @@
+package Sheduler;
+
+/**
+ * @author: Griffin Robinson
+ * @date: 2023-06-01
+ * @File Name: SupervisionDuty
+ * @description: _______
+ */
+public class SupervisionDuty extends Duty{
+
+        /**
+        * Instantiates a new Duty.
+        *
+        * @param time     the time
+        * @param location the location
+        * @param teacher the teacher
+        */
+        public SupervisionDuty(Time time, Location location, Teacher teacher) {
+            super(time, location, teacher);
+        }
+}
