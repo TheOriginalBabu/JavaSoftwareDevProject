498ee20cbabc6c1e4fe28c32ae833c42820dc0d5
TheOriginalBabu <51098394+TheOriginalBabu@users.noreply.github.com>
2023-06-08 12:49:17 -0400

    window stuff

--- DutyScheduler/src/Scheduler/Window.java
+++ DutyScheduler/src/Scheduler/Window.java
@@ -2,6 +2,20 @@ package Scheduler;
 
 import javax.swing.*;
 
-public class Window {
+public class Window extends JFrame{
     private JPanel BaseFrame;
+    private JPanel Menu;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+
+
+    public Window() {
+        setContentPane(BaseFrame);
+        setTitle("Duty Scheduler");
+        setSize(630, 855);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
